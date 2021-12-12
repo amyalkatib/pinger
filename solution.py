6c58ada884bf28b75397349f1d58a64f49470c53
amyalkatib <91368588+amyalkatib@users.noreply.github.com>
2021-12-11 22:02:59 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -116,7 +116,7 @@ def ping(host, timeout=1):
     for i in range(0,4):
         delay = doOnePing(dest, timeout)
         #delays[i] = delay
-        delays.append(int(delay)
+        delays.append(int(delay))
         # print(delay)
         time.sleep(1)  # one second
     
