349d5b99ab5135c214350b00d2a7e9beffa8d58f
amyalkatib <91368588+amyalkatib@users.noreply.github.com>
2021-12-11 21:32:32 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -119,20 +119,20 @@ def ping(host, timeout=1):
         delays.append(delay)
         # print(delay)
         time.sleep(1)  # one second
-        i += 1
+    
     packet_min = (delay)
     packet_max = (delay)
     sum = 0
-    i = 0
+   
     for i in range(0, 4):
         if  delays[i] < packet_min:
             packet_min = delays[i]
             sum = sum + packet_min
-            i = i + 1
+          
         if  delays[i] > packet_max:
             packet_max = delays[i]
             sum = sum + packet_max
-            i = i + 1
+       
 
     packet_avg = sum / 4
 
